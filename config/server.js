3350b36085ea3ba1989069f2c8f9ee598ee7515f
henriqueIFPE <hlf@discente.ifpe.edu.br>
2022-04-25 16:50:50 -0300

    controllerCategorias

--- config/server.js
+++ config/server.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const connectDatabase = require('../database/connect')
-//const categoriaController = require('../src/controllers/CategoriaController');
+const router = require('../src/routes/routes')
+const categoriaController = require('../src/controllers/CategoriaController');
 
 
 const app = express()
+
+app.use(router);
+//chamado todos os exports do Categoria controllers
+app.use(categoriaController);
+
+
 app.use(express.json());
 
 connectDatabase()
 
 dotenv.config();
-
-// app.get('/', (req , res)=>{
-//     res.send('<h1> Servidor rodando </h1>')
-// })
-require('../src/controllers/CategoriaController');
 
 
 const port = 3000
--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -1,8 +1,68 @@
 const express = require('express');
+const { json } = require('express/lib/response');
 const app = express()
 
-//const CategoriaModel = require('../models/CategoriaModel');
+app.use(express.json())
 
-app.get('/', (req , res)=>{
-    res.send('<h1> Servidor rodando </h1>')
+const CategoriaModel = require('../models/CategoriaModel');
+
+//Selecionar todas as categorias
+module.exports =  app.get('/categoria', async (req , res)=>{
+try {
+    const categoria = await CategoriaModel.find({});
+        
+    res.status(200).json(categoria)
+} catch (error) {
+    return res.status(500).send(error.message)
+}
+});
+
+//cadastrar Categorias
+module.exports =  app.post('/categoria/cadastrar', async (req, res)=>{
+    try {
+        const categoria = await CategoriaModel.create(req.body);
+        
+        res.status(201).json(categoria);
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+});
+
+//Buscar categorias  por ID
+module.exports = app.get('/categoria/:id', async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const categoria = await CategoriaModel.findById(id);
+
+        return res.status(200).json(categoria);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+//Update
+module.exports = app.patch('/categoria/:id', async (req,res)=>{
+    try {
+        const id = req.params.id;
+
+        const categoria = await CategoriaModel.findByIdAndUpdate(id, req.body, {new:true});
+
+        res.status(200).json(categoria);
+
+    } catch (error) {
+       return res.status(500).send(error.message);
+    }
+} )
+
+//deletando a categoria
+module.exports = app.delete('/categoria/:id', async (req,res)=>{
+    try {
+        const id = req.params.id;
+
+        const categoria = await CategoriaModel.findByIdAndRemove(id);
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
 })
+
